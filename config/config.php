e23b3468a91dc6870ef2b8c8b2fbab496af7be4c
vanminhcs <minhnguyenvancs213@gmail.com>
2026-04-24 14:46:19 +0700

    Thêm protocol để truy cập local

--- config/config.php
+++ config/config.php
@@ -1,4 +1,15 @@
 <?php
-// File: config/config.php
-define('BASE_URL', 'http://localhost/');
+$protocol = (!empty($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off' || $_SERVER['SERVER_PORT'] == 443) ? "https://" : "http://";
+
+$host = $_SERVER['HTTP_HOST'];
+
+$scriptName = str_replace('\\', '/', $_SERVER['SCRIPT_NAME']);
+$projectPath = str_replace('index.php', '', $scriptName);
+
+define('BASE_URL', $protocol . $host . $projectPath);
+
+define('DB_HOST', 'localhost');
+define('DB_USER', 'root');      
+define('DB_PASS', '');          
+define('DB_NAME', 'bk88');   
 ?>
